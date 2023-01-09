06580c2701f21c2003d2d3fe53e961c6a5ffc683
Adza <adriza.acharya@dghub.io>
2023-01-09 14:46:46 +0545

    initialized event page

---
+++ EventVerse/Controllers/EventController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventVerse.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
